bbb92a97ff717abef521d6889b97612a1639aca8
mkilmurray19-dotcom <m.kilmurray19@gmail.com>
2026-04-13 17:40:15 +1000

    Created DomainScraper class for scraping properties from domain.com.au

---
+++ scrapers/domain.py
@@ -0,0 +1,11 @@
+class DomainScraper:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def get_properties(self, search_criteria):
+        # TODO: Implement the logic to scrape properties based on the search criteria from domain.com.au
+        pass
+
+    def parse_properties(self, raw_data):
+        # TODO: Implement the logic to parse the scraped raw data into structured property details
+        pass
